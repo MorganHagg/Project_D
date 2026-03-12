6b66ede81b2a696c90860c66289be3b1de5829bc
Morgan Hägg <morganchane@live.no>
2026-03-12 15:54:11 +0100

    Started AI
    Started creating the AI personality system.

---
+++ Source/Project_D/NPC/EnemyBase.h
@@ -0,0 +1,37 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "EnemyBase.generated.h"
+
+// Struct
+USTRUCT(BlueprintType)
+struct FPersonality
+{
+	GENERATED_BODY()
+
+	float Aggressive;
+	float Defensive;
+	float Tactical;
+};
+
+UCLASS()
+class PROJECT_D_API AEnemyBase : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	AEnemyBase();
+	void GeneratePersonality();
+	void SetPersonality(int Aggression, float Defense, float Tactic);
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	virtual void Tick(float DeltaTime) override;
+
+	// Variables
+	FPersonality Personality;
+	
+};
